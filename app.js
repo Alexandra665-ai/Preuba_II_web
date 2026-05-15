348349af5d49e0a2c915c209d03910c82cd23661
Alexandra665-ai <alexandra.m.t.665@gmail.com>
2026-05-15 00:32:35 -0400

    faltó completar llave de cierre en app.js

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-
 const express = require("express");
-
 const animalRoutes = require("./routes/animalRoutes");
 
 const app = express();
-
 const puerto = 3000;
 
 app.use(express.json());
 
-app.use("/", animalRoutes);
-
 app.get("/", function (req, res) {
-
-    res.send("Servidor funcionando. Ingresa a /animales para ver los datos.");
-
+    res.send("Servidor ok. Ingresa a http://localhost:3000/animales para ver los datos.");
 });
 
-app.use(function (req, res) {
+app.use("/", animalRoutes);
 
+app.use(function (req, res) {
     res.status(404).json({
         ok: false,
         mensaje: "Ruta no encontrada"
     });
-
 });
-
 
 app.listen(puerto, function () {
     console.log("Servidor corriendo en http://localhost:" + puerto);
