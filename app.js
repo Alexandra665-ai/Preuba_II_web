e0c9f4d8d86852a279eb6b1a6d4ddec27d3ac6b6
Alexandra665-ai <alexandra.m.t.665@gmail.com>
2026-05-14 22:22:43 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const express = require("express");
+
+const animalRoutes = require("./routes/animalRoutes");
+
+const app = express();
+
+const puerto = 3000;
+
+app.use(express.json());
+
+app.use("/", animalRoutes);
+
+app.get("/", function (req, res) {
+
+    res.send("Servidor funcionando. Ingresa a /animales para ver los datos.");
+
+});
+
+app.use(function (req, res) {
+
+    res.status(404).json({
+        ok: false,
+        mensaje: "Ruta no encontrada"
+    });
+
+});
+
+
+app.listen(puerto, function () {
