1fe9e8a13a81943978e0b8887a6130892b641222
Alexandra665-ai <alexandra.m.t.665@gmail.com>
2026-05-14 22:05:33 -0400

    ruta animales conectada al controlador

--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -1,14 +1,13 @@
-// Importamos express para crear rutas
+
 const express = require("express");
 
-// Creamos el router de express
+
 const router = express.Router();
 
-// Importamos el controlador de animales
 const animalController = require("../controllers/animalController");
 
-// Creamos la ruta GET /animales
+
 router.get("/animales", animalController.listarAnimales);
 
-// Exportamos el router para usarlo en app.js
+
 module.exports = router;
